e11e82945efcbb9b4f07fc28a4088552c1663b98
Mitch Harrison <mitch@Mitchs-MBP.attlocal.net>
2022-06-25 00:54:17 -0400

    exercise 2_1

---
+++ book-of-r-exercises/2_1.R
@@ -0,0 +1,18 @@
+a <- 2.3
+answer1 <- ((6*a)+42) / 3^(4.2-3.62)
+print(answer1)
+
+# answer2 is (i)
+
+b <- c(25.2, 15, 16.44, 15.3, 18.6)
+answer3 <- sqrt(mean(b))
+print(answer3)
+
+answer4 <- log(0.3)
+print(answer4)
+
+answer5 <- exp(answer4)
+print(answer5)
+
+answer6 <- -0.00000000423546322
+print(answer6)
